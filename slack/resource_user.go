a1a4bd6d2d6a68e79ab18abcee35803b34a4225c
Hoang Mirs <hoang.mirs@gmail.com>
2023-08-17 09:58:36 +0700

    [#9] Add resource functions

--- slack/resource_user.go
+++ slack/resource_user.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/slack-go/slack"
@@ -11,10 +12,10 @@ import (
 
 func resourceSlackUser() *schema.Resource {
 	return &schema.Resource{
-		ReadContext: resourceSlackUserRead,
-		// CreateContext: resourceSlackUserGroupCreate,
-		// UpdateContext: resourceSlackUserGroupUpdate,
-		// DeleteContext: resourceSlackUserGroupDelete,
+		ReadContext:   resourceSlackUserRead,
+		CreateContext: resourceSlackUserCreate,
+		UpdateContext: resourceSlackUserUpdate,
+		DeleteContext: resourceSlackUserDelete,
 
 		Importer: &schema.ResourceImporter{
 			StateContext: schema.ImportStatePassthroughContext,
@@ -26,9 +27,17 @@ func resourceSlackUser() *schema.Resource {
 				Required: true,
 				ForceNew: true,
 			},
-			"name": {
+			"first_name": {
 				Type:     schema.TypeString,
 				Optional: true,
+			},
+			"last_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"team_id": {
+				Type:     schema.TypeString,
+				Required: true,
 			},
 		},
 	}
@@ -36,14 +45,15 @@ func resourceSlackUser() *schema.Resource {
 
 func resourceSlackUserRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	client := m.(*slack.Client)
-	id := d.Id()
 	var diags diag.Diagnostics
+
+	id := d.Id()
 
 	user, err := client.GetUserByEmailContext(ctx, id)
 	if err != nil {
 		diags = append(diags, diag.Diagnostic{
 			Severity: diag.Warning,
-			Summary:  fmt.Sprintf("user %s not found", id),
+			Summary:  fmt.Sprintf("error getting user %s: %s", id, err.Error()),
 		})
 		d.SetId("")
 
@@ -54,5 +64,40 @@ func resourceSlackUserRead(ctx context.Context, d *schema.ResourceData, m interf
 	d.Set("name", user.Name)
 	d.SetId(user.Profile.Email)
 
+	return nil
+}
+
+func resourceSlackUserCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	// client := m.(*slack.Client)
+	// var diags diag.Diagnostics
+
+	// email := d.Get("email").(string)
+	// name := d.Get("name").(string)
+
+	// if d.
+
+	// user, err := client.GetUserByEmailContext(ctx, email)
+	// if err != nil {
+	// 	tflog.Info(ctx, fmt.Sprintf("Error getting user by email %s: %s", email, err.Error()))
+	// }
+
+	// if user != nil {
+	// 	diags = append(diags, diag.Diagnostic{
+	// 		Severity: diag.Warning,
+	// 		Summary:  fmt.Sprintf("user with email %s already exists", email),
+	// 	})
+	// 	d.SetId(email)
+
+	// 	return diags
+	// }
+
+	// _, err = client.InviteToTeam()
+}
+
+func resourceSlackUserUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	return nil
+}
+
+func resourceSlackUserDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	return nil
 }
